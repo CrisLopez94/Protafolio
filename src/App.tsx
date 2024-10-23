a8ecd2a93692c84d470a6d105f127be79dcc83f5
CrisLopez94 <clopez@ppf.com.hn>
2024-10-23 08:07:50 -0600

    Inicio Proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <header className="bg-indigo-400 py-4">
+        <h1 className="text-center text-4xl text-white">Portafolio</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
